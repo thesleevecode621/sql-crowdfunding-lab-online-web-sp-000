3f574290cea803e662056e4b141b66c024815442
boruch d <dubywider@gmail.com>
2019-10-25 19:12:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,10 @@
-create table projects(id integer PRIMARY key)
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
 create table users (id integer PRIMARY KEY,name text, age integer)
 
